b6587a47a567b90edfcb14e0a8fa7e2b2a481cd3
Opzo <74737204+Opzo18@users.noreply.github.com>
2024-09-14 19:41:37 +0200

    a

--- assets/js/applyTheme.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-
-
-  // Apply theme from localStorage
-  const themeLink = document.getElementById("theme-link");
-  const currentTheme = localStorage.getItem("theme") || "light";
-  themeLink.setAttribute("href", currentTheme === "dark" ? "../assets/css/dark-theme.css" : "../assets/css/light-theme.css");
-});
